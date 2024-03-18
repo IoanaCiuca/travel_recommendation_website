5993382303f2682c0e2fb07a203d9dd94f216ab2
IoanaCiuca <ioanamciuca@yahoo.com>
2024-03-18 07:28:16 -0400

    add search logic

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,30 +1,42 @@
 function showResults() {
     const input = document.getElementById('recommendationInput').value.toLowerCase();
     const resultDiv = document.getElementById('result');
+    const countriesKeywords = ["countries", "country"];
+    const templesKeywords = ["temple", "temples"];
+    const beachesKeywords = ["beach", "beaches"];
+    let countriesFlag = 0, templesFlag = 0, beachesFlag = 0;
+    let inputResult = '';
     resultDiv.innerHTML = '';
 
+    if(countriesKeywords.find(item => item === input.toLowerCase())) {
+        countriesFlag = 1;
+    } else if(templesKeywords.find(item => item === input.toLowerCase())) {
+        templesFlag = 1;
+    } else if(beachesKeywords.find(item => item === input.toLowerCase())) {
+        beachesFlag = 1;
+    } else {
+        resultDiv.innerHTML = 'Please use an appropiate keyword.';
+        return;
+    }
     fetch('travel_recommendation_api.json')
       .then(response => response.json())
       .then(data => {
-        console.log(data);
-        const result = data.countries.find(item => item.toLowerCase() === input);
-        console.log(result);
-
-        /*if (country) {
-          const cities = country.cities.join(', ');*/
-          /*const citiesName = country.cities.name.join(', ');
-          const citiesImg = country.cities.imageUrl.join(', ');
-          const citiesDescription = country.cities.description.join(', ');*/
-
-          //resultDiv.innerHTML += `<h2>${cities.name}</h2>`;
-          /*resultDiv.innerHTML += `<h3>${citiesName}</h3>`
-          resultDiv.innerHTML += `<img src="${citiesImg}" alt="hjh">`;
-
-          resultDiv.innerHTML += `<p><strong>Description:</strong> ${citiesDescription}</p>`;*/
-          //console.log(cities.name);
-        /*} else {
-          resultDiv.innerHTML = 'Not found.';
-        }*/
+        if(countriesFlag) {
+            inputResult = data.countries;
+        } else if(templesFlag) {
+            inputResult = data.temples;
+        } else {
+            inputResult = data.beaches;
+        }
+        inputResult.forEach(element => {
+            resultDiv.innerHTML += `<h1>${element.name}</h1>`;
+            let citiesArr = element.cities;
+            citiesArr.forEach(city => {
+                resultDiv.innerHTML += `<img src="${city.imageUrl}" alt="hjh" style="width: 20%">`;
+                resultDiv.innerHTML += `<p><strong>${city.name}</strong></p>`;
+                resultDiv.innerHTML += `<p>${city.description}</p>`;
+            });
+        });
       })
       .catch(error => {
         console.error('Error:', error);
